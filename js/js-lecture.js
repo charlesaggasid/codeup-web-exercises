c5e740bdf39815ccaace40404cd83004ce1c9768
Charles Aggasid <charles.aggasid01@gmail.com>
2021-10-08 10:49:09 -0500

    completed js html lecture

---
+++ js/js-lecture.js
@@ -0,0 +1,21 @@
+"use strict"
+
+var cj = "firstName";
+
+console.log(cj);
+
+console.log("Message alert lol");
+
+var returnOfAlert = alert("You high?");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("YEsssss");
+console.log(returnOfConfirm);
+
+var answer = prompt("Well let me sell you something! Type what you want.");
+console.log("Nice I have " + answer + "!"  )
+
+var leastFavoriteSoda = prompt("What is your least favorite soda?");
+
+alert("I hate " + leastFavoriteSoda + "!")
+
