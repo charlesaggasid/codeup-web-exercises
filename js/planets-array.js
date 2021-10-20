f79efab83b48a82ab639b935b2fb5b6a5f67432a
Charles Aggasid <charles.aggasid01@gmail.com>
2021-10-19 20:27:26 -0500

    Done with planets-array

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,26 +18,33 @@
      * the step that it describes
      */
 
+    var theSun = planets.unshift('The Sun')
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
 
+    var pluto = planets.push('Pluto')
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
+    var removeSun = planets.shift('Removing "The Sun"')
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
 
+    var removePluto = planets.pop('Removing Pluto')
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
+    var indexEarth = planets.indexOf('Earth')
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(indexEarth);
 
     console.log("Reversing the order of the planets array.");
-    console.log(planets);
+    console.log(planets.reverse());
 
     console.log("Sorting the planets array.");
-    console.log(planets);
+    console.log(planets.sort());
 
-    
+
+    console.log(planets)
 
 })();
